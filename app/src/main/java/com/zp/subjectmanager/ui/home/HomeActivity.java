2a48b572ee0829c9d7f13031051d9c4daac30d08
zp <253908058@qq.com>
2019-01-10 15:57:17 +0800

    create

---
+++ app/src/main/java/com/zp/subjectmanager/ui/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.zp.subjectmanager.ui.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zp.subjectmanager.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_layout);
+    }
+}
